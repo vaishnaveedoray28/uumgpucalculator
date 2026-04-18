9370d8f2575c8a547e320ef9fe1e76e65c9c26c4
vaishnaveedoray28 <vaishnaveedoray28@gmail.com>
2026-04-18 15:41:30 +0800

    homepage edited

--- uumgpucalculator/lib/homepage.dart
+++ uumgpucalculator/lib/homepage.dart
@@ -1,3 +1,41 @@
 import 'package:flutter/material.dart';
 import 'package:audioplayers/audioplayers.dart';
 import 'package:google_fonts/google_fonts.dart';
+
+class GPAHome extends StatefulWidget {
+  const GPAHome({super.key});
+
+  @override
+  State<GPAHome> createState() => _GPAHomeState();
+}
+
+class _GPAHomeState extends State<GPAHome> {
+  final player = AudioPlayer();
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('UUM GPU Calculator', style: GoogleFonts.poppins(fontSize: 20, fontWeight: FontWeight.w600),),
+        centerTitle: true,
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text('Welcome to UUM GPU Calculator', style: GoogleFonts.poppins(fontSize: 18, fontWeight: FontWeight.w500),),
+            const SizedBox(height: 20,),
+            ElevatedButton(
+              onPressed: () async {
+                await player.play(AssetSource('audio/welcome.mp3'));
+              }, 
+              child: Text('Play Welcome Audio', style: GoogleFonts.poppins(fontSize: 16, fontWeight: FontWeight.w500),),
+            ),
+          ],
+        ),
+      ),
+    );
+  }
+}
+
+
+
